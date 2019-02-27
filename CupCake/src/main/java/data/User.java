cd2bc85d661dc6371cd0424ee0dc7b63f08dbfc4
Andreas Vikke <vikkedesign@gmail.com>
2019-02-27 23:15:25 +0100

    Added email to User

--- CupCake/src/main/java/data/User.java
+++ CupCake/src/main/java/data/User.java
@@ -2,21 +2,27 @@ package data;
 
 /**
  *
- * @author Martin Frederiksen
+ * @author Martin Frederiksen && Andreas Vikke
  */
 public class User {
     private String username;
+    private String email;
     private String password;
     private double balance;
 
-    public User(String username, String password, double balance) {
+    public User(String username, String email, String password, double balance) {
         this.username = username;
+        this.email = email;
         this.password = password;
         this.balance = balance;
     }
 
     public String getUsername() {
         return username;
+    }
+    
+    public String getEmail() {
+        return email;
     }
 
     public String getPassword() {
