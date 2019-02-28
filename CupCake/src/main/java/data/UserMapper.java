32730a35dd8e8a62041ce2eb3527f0b438ec2801
Andreas Vikke <vikkedesign@gmail.com>
2019-02-28 10:12:00 +0100

    Moved user

--- CupCake/src/main/java/data/UserMapper.java
+++ CupCake/src/main/java/data/UserMapper.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import sun.net.ftp.FtpReplyCode;
+import presentation.model.User;
 
 /**
  *
