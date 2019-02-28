5eea0ec6c9e4dd64422c3289ff361530ba580b58
Asger H. Sørensen <znaszger@hotmail.com>
2019-02-28 12:36:21 +0100

    LoginServlet

--- CupCake/src/main/java/presentation/view/LoginCommand.java
+++ CupCake/src/main/java/presentation/view/LoginCommand.java
@@ -5,6 +5,9 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import logic.UserController;
+import logic.ValidateUserController;
 
 /**
  *
@@ -14,18 +17,42 @@ public class LoginCommand extends Command {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Cupcake</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>CupCake Login Page</h1>");
-                out.println("</body>");
-                out.println("</html>");
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        ValidateUserController vuc = new ValidateUserController();
+        UserController uc = new UserController();
+        
+        boolean valid = vuc.validateUser(username, password);
+
+        uc.getUser(username);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Cupcake</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>CupCake Login Page</h1>");
+            out.println("Username: <br>");
+            out.println("<input type=\"text\" name=\"UsernameLogin\"><br>");
+            out.println("Password : <br>");
+            out.println("<input type=\"password\" name=\"PasswordLogin\"><br>");
+            out.println("<input type=\"submit\" name=\"LoginLogin\" value=\"Login\"> <br>");
+            if (valid) {
+                out.println("<h1>You are now logged in.</h1>");
+            } else {
+                out.println("<h1>Incorrect username and/or password</h1>");
             }
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
 }
---
+++ CupCake/src/main/java/presentation/view/LoginServlet.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation.view;
+
+import logic.ValidateUserController;
+import logic.UserController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Asger Hermind Sørensen
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        ValidateUserController vuc = new ValidateUserController();
+        UserController uc = new UserController();
+        boolean valid = vuc.validateUser(username, password);
+
+        
+        uc.getUser(username);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("Username: <br>");
+            out.println("<input type=\"text\" name=\"UsernameLogin\"><br>");
+            out.println("<br>Password : <br>");
+            out.println("<input type=\"password\" name=\"PasswordLogin\"><br>");
+            out.println("<input type=\"submit\" name=\"LoginLogin\" value=\"Login\"> <br>");
+            if(valid){
+                out.println("<h1>You are now logged in.</h1>");
+            }else{
+                out.println("<h1>Incorrect username and/or password</h1>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
