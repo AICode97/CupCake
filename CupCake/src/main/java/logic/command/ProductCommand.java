c38503e70a99aaf737633a9716bf6806d8305741
Krigermus <zyjinxx@hotmail.com>
2019-03-05 16:18:21 +0100

    ProductCommand fixed

--- CupCake/src/main/java/logic/command/ProductCommand.java
+++ CupCake/src/main/java/logic/command/ProductCommand.java
@@ -16,38 +16,25 @@ import logic.model.enums.CupcakePartEnum;
  *
  * @author Martin Frederiksen
  */
-public class ProductCommand extends Command{
+public class ProductCommand extends Command {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CupcakeController cc = new CupcakeController();     
+        CupcakeController cc = new CupcakeController();
         CupcakePart bottom = cc.getCupcakePart(CupcakePartEnum.BOTTOM, Integer.parseInt(request.getParameter("bottom")));
         CupcakePart top = cc.getCupcakePart(CupcakePartEnum.TOP, Integer.parseInt(request.getParameter("top")));
         int qty = Integer.parseInt(request.getParameter("qty"));
-        
-        ShoppingCart sc = new ShoppingCart();
-        sc.setLineItem(new LineItem(bottom, top, qty));
-        
         HttpSession session = request.getSession();
-        session.setAttribute("ShoppingCart", sc);
-        
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProductControl</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>LineItems</h1>");
-            for(LineItem li : sc.getLineItems()){
-                out.println("<p>Bund: "+ li.getBottom().getName() + "</p>"); 
-                out.println("<p>Top: "+ li.getTop().getName() + "</p>");
-                out.println("<p>Mængde: "+ li.getQuantity()+ "</p>");
-                out.println("<p>Pris: " + sc.calculate());
-            }
-            out.println("</body>");
-            out.println("</html>");
+
+        if (session.getAttribute("ShoppingCart") != null) {
+            ShoppingCart sc = (ShoppingCart) session.getAttribute("ShoppingCart");
+            sc.setLineItem(new LineItem(bottom, top, qty));
+            System.out.println("Set shoppingcart session again?");
+            //session.setAttribute("ShoppingCart", sc);
+        } else {
+            ShoppingCart sc = new ShoppingCart();
+            sc.setLineItem(new LineItem(bottom, top, qty));
+            session.setAttribute("ShoppingCart", sc);
         }
     }
 
