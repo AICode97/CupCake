7a4bcfc34f873d05d6eb56b9f0c3a5cf4ec97d4c
Krigermus <zyjinxx@hotmail.com>
2019-03-05 18:34:33 +0100

    registercommand import

--- CupCake/src/main/java/logic/command/RegisterCommand.java
+++ CupCake/src/main/java/logic/command/RegisterCommand.java
@@ -1,7 +1,6 @@
 package logic.command;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
