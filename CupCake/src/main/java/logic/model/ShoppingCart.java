88cf09ce50ed4628eac986d2053b18628f27a841
Krigermus <zyjinxx@hotmail.com>
2019-03-05 17:53:06 +0100

    product/shoppingcart changes

--- CupCake/src/main/java/logic/model/ShoppingCart.java
+++ CupCake/src/main/java/logic/model/ShoppingCart.java
@@ -10,6 +10,7 @@ import logic.CupcakeController;
  * @author Martin Frederiksen
  */
 public class ShoppingCart {
+
     private List<LineItem> lineItems;
 
     public ShoppingCart() {
@@ -21,32 +22,22 @@ public class ShoppingCart {
     }
 
     public void addLineItem(LineItem li) {
-        List<LineItem> items = getLineItems();
-        List<LineItem> cloneItems = new ArrayList(items.size());
-        for(LineItem l : items){
-            cloneItems.add(l);
-        }
-        if (items.isEmpty()) {
-            lineItems.add(li);
-        } else {
-            for (LineItem i : cloneItems) {
-                if (i.getBottom().getName().equals(li.getBottom().getName()) && i.getTop().getName().equals(li.getTop().getName())) {
-                    i.addQuantity(li.getQuantity());
-                    lineItems = cloneItems;
-                }else {
-                    lineItems.add(li);
-                }
+        for (LineItem i : lineItems) {
+            if (li.getBottom().getId() == i.getBottom().getId() && li.getTop().getId() == i.getTop().getId()) {
+                i.addQuantity(li.getQuantity());
+                return;
             }
         }
+        lineItems.add(li);
     }
-    
-    public int calculate(){
+
+    public int calculate() {
         List<LineItem> li = lineItems;
         int price = 0;
-        for(LineItem i : li){
-            price += i.getPrice() * i.getQuantity();
+        for (LineItem i : li) {
+            price += i.getPrice();
         }
-        
+
         return price;
     }
 
@@ -54,7 +45,10 @@ public class ShoppingCart {
         ShoppingCart sc = new ShoppingCart();
         CupcakeController cc = new CupcakeController();
         sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 3), cc.getCupcakePart(CupcakePartEnum.TOP, 4), 1));
-        sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 3), cc.getCupcakePart(CupcakePartEnum.TOP, 4), 2));
+        sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 1), cc.getCupcakePart(CupcakePartEnum.TOP, 1), 2));
+        sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 3), cc.getCupcakePart(CupcakePartEnum.TOP, 4), 1));
+        sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 1), cc.getCupcakePart(CupcakePartEnum.TOP, 1), 2));
+        sc.addLineItem(new LineItem(cc.getCupcakePart(CupcakePartEnum.BOTTOM, 1), cc.getCupcakePart(CupcakePartEnum.TOP, 1), 2));
         for (LineItem item : sc.getLineItems()) {
             System.out.println(item.getQuantity());
         }
