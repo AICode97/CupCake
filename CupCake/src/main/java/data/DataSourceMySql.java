939c5475418cadaed2f7ef4b7f273de5fe449804
Andreas Vikke <vikkedesign@gmail.com>
2019-03-11 11:41:39 +0100

    Added JavaDoc

--- CupCake/src/main/java/data/DataSourceMySql.java
+++ CupCake/src/main/java/data/DataSourceMySql.java
@@ -9,17 +9,26 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 public class DataSourceMySql {
 
     private MysqlDataSource dataSource = new MysqlDataSource();
-    
-    public DataSourceMySql(){
+    private MysqlDataSource localDataSource = new MysqlDataSource();
+
+    public DataSourceMySql() {
         {
-            try{
             dataSource.setServerName("andreasvikke.dk");
-            dataSource.setPort(3306);
-            dataSource.setDatabaseName("cupcake");
-            dataSource.setUser("transformer");
-            dataSource.setPassword("f7qGtArm");
-            dataSource.setUseSSL(false);
-            } catch(Exception ex){
+            try {
+                dataSource.setServerName("andreasvikke.dk");
+                dataSource.setPort(3306);
+                dataSource.setDatabaseName("cupcake");
+                dataSource.setUser("transformer");
+                dataSource.setPassword("f7qGtArm");
+                dataSource.setUseSSL(false);
+                
+                localDataSource.setServerName("localhost");
+                localDataSource.setPort(3306);
+                localDataSource.setDatabaseName("cupcake");
+                localDataSource.setUser("transformer");
+                localDataSource.setPassword("f7qGtArm");
+                localDataSource.setUseSSL(false);
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
