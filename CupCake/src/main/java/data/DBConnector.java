dddb22b8cadd8564f3b13ff4215cb9ff525528d8
AICode97 <56427790+AICode97@users.noreply.github.com>
2020-03-28 12:59:12 +0100

    UserList
    op og køre

--- CupCake/src/main/java/data/DBConnector.java
+++ CupCake/src/main/java/data/DBConnector.java
@@ -13,8 +13,8 @@ import java.util.StringJoiner;
 public class DBConnector {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://andreasvikke.dk:3306/cupcake";
-    private static final String USER = "transformer";
-    private static final String PASSWORD = "f7qGtArm";
+    private static final String USER = "root";
+    private static final String PASSWORD = "klon3903202";
     private static Connection connection = null;
 
     public static Connection getConnection() {
